969c4c4b53db4e3bc59be9e97456fc29593c4e03
LordCail-Unity <114961576+LordCail-Unity@users.noreply.github.com>
2022-10-18 14:11:32 +1100

    Added Lava sinking effect
    PlayerCollision script

--- MicroGame-2022-10/Assets/Scripts/PlayerCollision.cs
+++ MicroGame-2022-10/Assets/Scripts/PlayerCollision.cs
@@ -4,11 +4,14 @@ public class PlayerCollision : MonoBehaviour
 {
 
     private PlayerController _playerController;
+    private Rigidbody _rigidbody;
 
     private void Awake()
     {
         _playerController = this.GetComponent<PlayerController>();
         Debug.Log("Player Collision: Player Controller locked and loaded");
+        _rigidbody = this.GetComponent<Rigidbody>();
+        Debug.Log("Player Collision: Player Rigidbody locked and loaded");
     }
 
     private void OnCollisionEnter(Collision collidedObject)
@@ -17,7 +20,7 @@ public class PlayerCollision : MonoBehaviour
         {
             _playerController.enabled = false;
             Debug.Log("Movement Disabled: Obstacle");
-            // Transition to next scene
+            // RESTART LEVEL
         }
     }
 
@@ -28,6 +31,15 @@ public class PlayerCollision : MonoBehaviour
             _playerController.enabled = false;
             Debug.Log("Movement Disabled: Finish");
             // Transition to next scene
+        }
+        if (triggerCollider.tag == "Killzone")
+        {
+            _playerController.enabled = false;
+            Debug.Log("Movement Disabled: Killzone");
+            _rigidbody.constraints = RigidbodyConstraints.FreezeRotation;
+            _rigidbody.drag = 20;
+            Debug.Log("Movement Disabled: Killzone");
+            // RESTART LEVEL
         }
     }
 
